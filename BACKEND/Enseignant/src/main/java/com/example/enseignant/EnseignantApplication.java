41ad21ab201da3b47c5f20ae32660eb6c67b240b
kolsiah <101052716+kolsiah@users.noreply.github.com>
2025-02-22 00:10:07 +0100

    initial

---
+++ BACKEND/Enseignant/src/main/java/com/example/enseignant/EnseignantApplication.java
@@ -0,0 +1,13 @@
+package com.example.enseignant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnseignantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnseignantApplication.class, args);
+    }
+
+}
---
+++ BACKEND/OffreStage/src/test/java/com/example/offrestage/OffreStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offrestage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffreStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
