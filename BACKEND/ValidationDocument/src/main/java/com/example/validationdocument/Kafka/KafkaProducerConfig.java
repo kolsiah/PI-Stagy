2190e9aa6a3058e277393f38021740283aa58032
adam582 <adam.hachana@esprit.tn>
2025-02-25 22:48:01 +0100

    Developed ValidationDocument MS + KAFKA for events

---
+++ BACKEND/ValidationDocument/src/main/java/com/example/validationdocument/Kafka/KafkaProducerConfig.java
@@ -0,0 +1,29 @@
+package com.example.validationdocument.Kafka;
+
+import com.example.validationdocument.dto.ValidationEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import java.util.HashMap;
+import java.util.Map;
+@Configuration
+public class KafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, ValidationEvent> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, ValidationEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
---
+++ BACKEND/ValidationDocument/src/main/java/com/example/validationdocument/ValidationDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.example.validationdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationDocumentApplication.class, args);
+    }
+
+}
