0ae5cc0e7c5c1d2e36ccb8f2e13a993e8b06634a
adam582 <adam.hachana@esprit.tn>
2025-02-25 20:56:32 +0100

    Integrated API Gateway + Eureka server for Document MS

--- BACKEND/API-GATEWAY/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ BACKEND/API-GATEWAY/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,29 @@
 package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+		return  builder.routes()
+				.route("Candidat", r->r.path("/A/candidat/**")
+						.uri("http://localhost:8081"))
+				.route("Document", r ->r.path("/documents/**")
+						.uri("http://localhost:8087"))
+				.build();
+	}
 }
+
+
